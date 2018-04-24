e6a1e74e56610bdf0792548d963ca48c7c2ab063
awadomar34 <38463278+awadomar34@users.noreply.github.com>
2018-04-24 12:19:29 +0100

    added stuff
    added an advanced search pages, havent linked it yet, need to add topnav to it and the php file for the results has not been finished
    
    added a tooltip to the stylesheet, allows u to hover over a word for more info

---
+++ advanced_search.php
@@ -0,0 +1,12 @@
+<?php
+  require_once('globals.php');
+
+  $product_name=$_POST["title_AS"];
+  $category=$_POST["category_AS"];
+  $description=$_POST["description_AS"];
+  $min_price= $_POST["price-min"];
+  $max_price=$_POST["price-max"];
+  $postage=$_POST["postage_AS"];
+
+
+?>
